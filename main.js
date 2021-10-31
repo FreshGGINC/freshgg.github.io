16942fead1a509d2420987a2fbcaba27f6232526
TonyChamoun7 <cooltony7@hotmail.com>
2021-10-31 15:03:01 -0400

    worked on pricing page

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ function addCart_one(){
     }
     else{
         y += 4.99;
+        y = y.toFixed(2);
         y1 += 4.99 * 0.13;
+        y1 = y1.toFixed(2);
         y2 += 4.99 * 1.13;
+        y2 = y2.toFixed(2);
         x1 += 1;
         document.getElementById('price').innerHTML = "Sub-Total: " + y + "$";
         document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
@@ -18,12 +21,16 @@ function addCart_one(){
     }
 }
 function addCart_two(){
+    
     if (x2 == 1 || x1 == 1){
     }
     else{
         y += 7.99;
+        y = y.toFixed(2);
         y1 += 7.99 * 0.13;
+        y1 = y1.toFixed(2);
         y2 += 7.99 * 1.13;
+        y2 = y2.toFixed(2);
         x2 += 1;
         document.getElementById('price').innerHTML = "Sub-Total: " + y + "$";
         document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
@@ -34,10 +41,10 @@ function addCart_two(){
 function removeCart_one(){
     if (x1 == 0){
     }
-    else {
-        y -= 4.99;
-        y1 -= 4.99 * 0.13;
-        y2 -= 4.99 * 1.13;
+    else{
+        y -= y;
+        y1 -= y1;
+        y2 -= y2;
         x1 -= 1;
         document.getElementById('price').innerHTML = "Sub-Total: " + y + "$";
         document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
@@ -48,10 +55,10 @@ function removeCart_one(){
 function removeCart_two(){
     if (x2 == 0){
     }
-    else {
-        y -= 7.99;
-        y1 -= 7.99 * 0.13;
-        y2 -= 7.99 * 1.13;
+    else{
+        y -= y;
+        y1 -= y1;
+        y2 -= y2;
         x2 -= 1;
         document.getElementById('price').innerHTML = "Sub-Total: " + y + "$";
         document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
@@ -59,27 +66,3 @@ function removeCart_two(){
         document.getElementById('x2').innerHTML = x2;
     }
 }
-
-/*function addCart_tax_one(){
-    y1 += 5 * 0.13;
-    document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
-}
-function addCart_tax_two(){
-    y1 += 8 * 0.13;
-    document.getElementById('tax').innerHTML = "Tax: " + y1 + "$";
-}
-function addCart_total_one(){
-    y2 += 5 * 1.13;
-    document.getElementById('total').innerHTML = "Total: " + y2 + "$";
-}
-function addCart_total_two(){
-    y2 += 8 * 1.13;
-    document.getElementById('total').innerHTML = "Total: " + y2 + "$";
-}*/
-
-	
-
-
-//if(document.getElementById("PurchaseChoice1").checked,function() {
-    //document.getElementById("price").innerHTML = "Total: $" + 5;
-//})
